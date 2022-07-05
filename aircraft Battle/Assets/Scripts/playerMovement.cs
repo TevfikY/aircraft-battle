8635de714d9b224bf231bd7ea7547321033aa69d
TevfikY <s200201029@ankarabilim.edu.tr>
2022-07-05 14:49:59 +0300

    update 2
    prefab palyer added

---
+++ aircraft Battle/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 40f;
+    private Rigidbody2D rigidBody;
+    private bool isAlive = true;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void PlayerMovement()
+    {
+        if (isAlive)
+        {
+            
+        }
+            
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        rigidBody.velocity = new Vector2(xAxis * speed * Time.time, 0);
+
+
+    }
+    
+    
+}
