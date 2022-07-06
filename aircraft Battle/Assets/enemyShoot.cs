edd93d3d896b1724f46cd96d768929bd269dc387
TevfikY <s200201029@ankarabilim.edu.tr>
2022-07-06 16:47:01 +0300

    last update

---
+++ aircraft Battle/Assets/enemyShoot.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyShoot : MonoBehaviour
+{
+     float timeReseter ;
+    [SerializeField] float timeBetweenShots = 3f;
+    [SerializeField] float bulletPerSecond = 0.2f;
+     private float bulletPerSecondReseter;
+    [SerializeField] private GameObject enemyBullet;
+    [SerializeField] float  bulletSpeed = 300f;
+    private float bulletCount = 0;
+    void Start()
+    {
+        timeReseter = Time.time + 1f;
+        
+        bulletPerSecondReseter = Time.time;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (bulletCount < 3)
+        {
+            if (Time.time > timeReseter)
+            {
+                if (Time.time > bulletPerSecondReseter)
+                {
+                    GameObject bullet = Instantiate(enemyBullet, transform.GetChild(0).position,
+                        transform.GetChild(0).rotation);
+                    bullet.GetComponent<Rigidbody2D>().velocity = -1*Vector2.up*bulletSpeed*Time.deltaTime;
+                    bulletPerSecondReseter = Time.time + bulletPerSecond;
+                    bulletCount++;
+                }
+            }
+            
+        }
+        else
+        {
+            
+            timeReseter = Time.time + timeBetweenShots;
+            bulletCount = 0;
+        }
+            
+            
+            
+        
+    }
+
+    IEnumerator Shoot()
+    {
+        Debug.Log("shoot");
+        yield return new WaitForSecondsRealtime(0.2f);
+    }
+}
