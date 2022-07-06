42b2a9637c472288fbdd4aa023e137122b97b1da
Utku Bektaş Akbaba <utkuak2002@gmail.com>
2022-07-06 15:23:21 +0300

    tile looping

--- aircraft Battle/Assets/Scripts/levelManagement.cs
+++ aircraft Battle/Assets/Scripts/levelManagement.cs
@@ -2,21 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class levelManagement : MonoBehaviour
 {
     public GameObject[] tilePrefabs;
     public float zSpawn = 0;
-    public float tileLength = 30;
+    public Transform playerTransform;
 
-    private void Start()
+
+
+    private void Update()
     {
-        spawnTile(0);
+        if (playerTransform.position.y > zSpawn)
+        {
+            spawnTile(0);
+        }
     }
 
     public void spawnTile(int tileIndex)
     {
-        Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
-        zSpawn += tileLength;
+        GameObject a = Instantiate(tilePrefabs[tileIndex]);
+        a.transform.position = new Vector2(0, 10 + zSpawn);
+        zSpawn += 10;
     }
 }
