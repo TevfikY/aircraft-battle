80414f776154abb1749fc5e47b833cd84ebdd7d4
TevfikY <s200201029@ankarabilim.edu.tr>
2022-07-18 16:05:17 +0300

    update

--- aircraft Battle/Assets/Scripts/wavePositionOnScene.cs
+++ aircraft Battle/Assets/Scripts/wavePositionOnScene.cs
@@ -13,6 +13,6 @@ public class wavePositionOnScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position =  new Vector3(0,cameraPos.position.y-3,0);
+        transform.position =  new Vector3(5,cameraPos.position.y-3,0);
     }
 }
