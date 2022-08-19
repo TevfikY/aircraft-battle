b451dcb5be6458c2826f2b10323494e6dab4ffee
Ufuk Baktır <ufukbaktir@gmail.com>
2022-08-19 16:56:49 +0300

    UI Update 2

---
+++ aircraft Battle/Assets/SoundController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField] private Image soundOnIcon;
+    [SerializeField] private Image soundOffIcon;
+    private bool muted = false;
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+        UpdateButtonIcon();
+        AudioListener.pause = muted;
+    }
+
+    private void UpdateButtonIcon()
+    {
+        if (muted == false)
+        {
+            soundOnIcon.enabled = true;
+            soundOffIcon.enabled = false;
+        }
+        else
+        {
+            soundOnIcon.enabled = false;
+            soundOffIcon.enabled = true;
+            
+        }
+    }
+
+    
+    public void OnButtonPress()
+    {
+        if (muted == false)
+        {
+            muted = true;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            muted = false;
+            AudioListener.pause = false;
+        }
+        Save();
+        UpdateButtonIcon();
+    }
+
+    private void Load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+    }
+}
