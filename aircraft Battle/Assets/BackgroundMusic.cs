dfdcf47f4ab33115ff150be64be7742c38371e87
Ufuk Baktır <ufukbaktir@gmail.com>
2022-08-19 23:46:03 +0300

    UI Update 3

---
+++ aircraft Battle/Assets/BackgroundMusic.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BackgroundMusic : MonoBehaviour
+{
+  private static BackgroundMusic backgroundMusic;
+
+  private void Awake()
+  {
+
+    if (backgroundMusic == null)
+    {
+      backgroundMusic = this;
+      DontDestroyOnLoad(backgroundMusic);
+    }
+    else
+    {
+      
+        Destroy(gameObject); 
+    }
+  }
+}
