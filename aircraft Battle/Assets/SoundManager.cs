76dcd3a00d3e4f81347bbf9544d72f04d7b57d3b
TevfikY <92394864+TevfikY@users.noreply.github.com>
2022-08-20 13:30:43 +0300

    little update

--- aircraft Battle/Assets/SoundManager.cs
+++ aircraft Battle/Assets/SoundManager.cs
@@ -16,7 +16,7 @@ public class SoundManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+           //DontDestroyOnLoad(gameObject);
         }
 
         else
