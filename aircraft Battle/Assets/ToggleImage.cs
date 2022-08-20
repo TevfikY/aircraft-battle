b2edd34b34bfd93dfeb9e63a0b2396298fcf4698
TevfikY <92394864+TevfikY@users.noreply.github.com>
2022-08-20 20:13:33 +0300

    baba merhaba

--- aircraft Battle/Assets/ToggleImage.cs
+++ aircraft Battle/Assets/ToggleImage.cs
@@ -1,9 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.U2D.Animation;
+
+
+
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class ToggleImage : MonoBehaviour
 {
